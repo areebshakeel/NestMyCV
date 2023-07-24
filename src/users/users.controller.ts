22e295d565a67fe95c83e286d9f0487fe2b1b822
areebtakafukl <muhammad.areeb@takafulbazaar.pk>
2023-07-24 18:57:55 +0500

    create and update data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
-
+import { UserService } from './users.service';
 @Controller('auth')
 export class UsersController {
+    constructor(private userService:UserService){}
     @Post('/signup')
     createUser(@Body() body:CreateUserDto){
         console.log("Create user body -> ",body);
+        this.userService.create(body.email, body.password)
+        return "Account created"
     }
 }
  
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { create } from 'domain';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectRepository(User) private repo:Repository<User>){}
+    create(email:string, password:string){
+        const user = this.repo.create({email,password})
+        
+        return this.repo.save(user)
+    }
+    findOne(id:number){
+      return  this.repo.findOne(id);
+    }
+
+    find(){
+
+    }
+
+    update(id:number){
+
+    }
+
+    remove(id:number){
+
+    }
+}
